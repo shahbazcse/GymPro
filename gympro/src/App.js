d23c3722910b12475d4ab07659ef45acc3b54eb9
shahbazcse <cseshahbaz@gmail.com>
2023-12-06 20:58:52 +0530

    Added NonDesktopPage

--- gympro/src/App.js
+++ gympro/src/App.js
@@ -5,20 +5,26 @@ import Exercises from "./pages/Exercises";
 import Goals from "./pages/Goals";
 import Foods from "./pages/Foods";
 import Dashboard from "./pages/Dashboard";
+import NonDesktopPage from "./pages/NonDesktopPage";
 
 function App() {
   return (
-    <div className="App flex font-bold">
-      <NavBar />
-      <div className="w-[85%] bg-[#dcf8f5] text-black h-screen overflow-auto">
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/exercises" element={<Exercises />} />
-          <Route path="/goals" element={<Goals />} />
-          <Route path="/foods" element={<Foods />} />
-        </Routes>
+    <>
+      <div className="block xl:hidden h-screen">
+        <NonDesktopPage />
       </div>
-    </div>
+      <div className="hidden xl:flex h-screen App font-bold">
+        <NavBar />
+        <div className="w-[85%] bg-[#dcf8f5] text-black h-screen overflow-auto">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/exercises" element={<Exercises />} />
+            <Route path="/goals" element={<Goals />} />
+            <Route path="/foods" element={<Foods />} />
+          </Routes>
+        </div>
+      </div>
+    </>
   );
 }
 
